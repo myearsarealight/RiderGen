398fe2f8f9c9cb2e47039afccd31484d43a1968c
Elizabeth OLeary <betholeary@gmail.com>
2020-11-22 21:20:36 +0000

    Fixed opacity for disabled options section. Added sortabled functionality for options and sections. Added stereo button prototype for keys.

--- RiderGen/static/scripts/channels.js
+++ RiderGen/static/scripts/channels.js
@@ -1,12 +1,21 @@
+// Allow options to be rearranged through dragging and dropping
+
+$(function () {
+    $(".sortable").sortable();
+    $("sortable").disableSelection();
+});
+
 // Press buttons to toggle each section as selected and disable section's options if it isn't selected
 
 $(".section").click(function () {
     $(this).toggleClass("selected");
     if ($(this).hasClass("selected")) {
         $(this).siblings("fieldset").prop("disabled", false);
+        $(this).siblings("fieldset").children(".overlay").hide();
     }
     else {
         $(this).siblings("fieldset").prop("disabled", true);
+        $(this).siblings("fieldset").children(".overlay").show();
     }
 });
 
@@ -15,4 +24,19 @@ $(".section").click(function () {
 $(".options").click(function () {
     $(this).toggleClass("selected");
     $(this).next("fieldset").toggle();
+});
+
+// Toggle between mono and stereo button
+
+$(".st").click(function () {
+    if ($(this).hasClass("mono")) {
+        $(this).removeClass("mono")
+        $(this).addClass("stereo")
+        $(this).text("stereo")
+    }
+    else {
+        $(this).removeClass("stereo")
+        $(this).addClass("mono")
+        $(this).text("mono")
+    }
 });
