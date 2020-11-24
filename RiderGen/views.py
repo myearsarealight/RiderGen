f555dec95c53b2420f10e2efc3309c06f6ba3238
Elizabeth OLeary <betholeary@gmail.com>
2020-11-24 21:54:59 +0000

    Made a start on SQL stuff

--- RiderGen/views.py
+++ RiderGen/views.py
@@ -4,14 +4,29 @@ Routes and views for the flask application.
 
 import os
 import pypyodbc
+import sqlite3
 
-from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from flask import Flask, flash, jsonify, redirect, render_template, request, session, g
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import datetime
 from RiderGen import app
+
+# DATABASE = 'channelsdb.db'
+
+def get_db():
+    db = getattr(g, '_channelsdb', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_channelsdb', None)
+    if db is not None:
+        db.close() 
 
 @app.route('/')
 @app.route('/home')
@@ -40,8 +55,10 @@ def channels():
 
 @app.route('/channelsub', methods=["POST"])
 def channelsub():
-    """Renders the channel list generator page."""
+    """Renders the channel list inside the generator page."""
     # Make a variable for the form data and an empty channel list
+    db = sqlite3.connect('RiderGen/channelsdb.db')
+    
     req = request.form
     clist = []
     # Get the act name and contact details
@@ -50,5 +67,10 @@ def channelsub():
     contactdets=req.get("contactdets")
     # Get the instruments to make up the channels
     instruments = req.getlist("instrument")
-        
+    # i = 0
+    #for inst in instruments:
+    #    db.execute("INSERT INTO Instruments (inst_id) VALUES (?)", (instruments[i],))
+    #    i += 1
+    #db.commit()
+    #db.close()
     return jsonify({'data': render_template('/channelsub.html', act_name=actname, contact_name=contactname, contact_details=contactdets, channel_list=instruments)})
